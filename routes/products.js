0c1f3e6d6e0c6445f16e2f1d8e38595624260cd6
C1neks <baranowskimarcin7@gmail.com>
2022-01-02 19:42:02 +0100

    Fixes ProductService

--- routes/products.js
+++ routes/products.js
@@ -1,38 +1,43 @@
 import express from "express";
 import { uuid } from "uuidv4";
 
-import { productService } from "../services/ProductService.js";
+import { ProductService, ProductModel } from "../services/ProductService.js";
 
 const router = express.Router();
-
+const product = new ProductService();
 router.get("/", (req, res) => {
-  const product = new productService();
+  console.log(product.getProducts());
   res.send(product.getProducts());
 });
 
 router.post("/", (req, res) => {
   const myBody = req.body;
-  const product = new productService();
-  res.send(product.createProduct(myBody));
+
+  // console.log(myBody.name, myBody.price, myBody.description);
+
+  res.send(
+    product.createProduct(myBody.name, myBody.price, myBody.description)
+  );
 });
 //
-router.get("/:id", (req, res) => {
-  const id = req.params;
-  const singleProduct = new productService();
-  res.send(singleProduct.getProductById(id));
-});
-
-router.delete("/:id", (req, res) => {
-  const id = req.params;
-  const product = new productService();
-  res.send(product.deleteProduct(id));
-});
-
-router.patch("/:id", (req, res) => {
-  const id = req.params;
-  const body = req.body;
-  const product = new productService();
-  res.send(product.updateProduct(id, body));
-});
+// router.get("/:id", (req, res) => {
+//   const id = req.params.id;
+//   const singleProduct = new ProductService();
+//   res.send(singleProduct.getProductById(id));
+// });
+//
+// router.delete("/:id", (req, res) => {
+//   const id = req.params.id;
+//
+//   const product = new ProductService();
+//   res.send(product.deleteProduct(id));
+// });
+//
+// router.patch("/:id", (req, res) => {
+//   const id = req.params.id;
+//   const body = req.body;
+//   const product = new ProductService();
+//   res.send(product.updateProduct(id, body));
+// });
 
 export default router;
--- services/ProductService.js
+++ services/ProductService.js
@@ -1,43 +1,74 @@
 import { uuid } from "uuidv4";
-
-let products = [];
-export class productService {
-  getProducts() {
-    return products;
-  }
-
-  createProduct(req) {
-    products.push({ ...req, id: uuid() });
-
-    return products;
-  }
-
-  getProductById(req) {
-    const { id } = req;
-
-    return products.find((product) => product.id === id);
-  }
-  //
-  deleteProduct(req) {
-    const { id } = req;
-
-    products = products.filter((product) => product.id !== id);
-
-    return products;
-  }
-
-  updateProduct(req, body) {
-    const { id } = req;
-    const { name, price, description } = body;
-
-    const updatedProduct = products.find((product) => product.id === id);
-
-    if (name) updatedProduct.name = name;
-
-    if (price) updatedProduct.price = price;
-
-    if (description) updatedProduct.description = description;
-
-    return `Product with id ${id} updated!`;
+export class ProductModel {
+  constructor(name, price, description) {
+    this.name = name;
+    this.price = price;
+    this.description = description;
   }
 }
+
+export class ProductService {
+  constructor() {
+    this.items = [];
+  }
+  getProducts() {
+    return this.items;
+  }
+  createProduct(name, price, description) {
+    if (Array.isArray(this.items)) {
+      const newProduct = new ProductModel(name, price, description);
+      this.items = [...this.items, newProduct];
+    }
+    return this.items;
+  }
+}
+
+// const product = new ProductService();
+// const product2 = new ProductService();
+// product.createProduct(5, "haha");
+// product2.createProduct(20, "bbbb");
+// product.getProduct();
+// product2.getProduct();
+// let products = [];
+// export class ProductService {
+//   constructor() {}
+//
+//   getProducts() {
+//     return products;
+//   }
+//
+//   createProduct(body) {
+//     products.push({ ...body, id: uuid() });
+//
+//     return products;
+//   }
+//
+//   getProductById(productId) {
+//     const id = productId;
+//
+//     return products.find((product) => product.id === id);
+//   }
+//   //
+//   deleteProduct(productId) {
+//     const id = productId;
+//
+//     products = products.filter((product) => product.id !== id);
+//
+//     return products;
+//   }
+//
+//   updateProduct(productId, body) {
+//     const id = productId;
+//     const { name, price, description } = body;
+//
+//     const updatedProduct = products.find((product) => product.id === id);
+//
+//     if (name) updatedProduct.name = name;
+//
+//     if (price) updatedProduct.price = price;
+//
+//     if (description) updatedProduct.description = description;
+//
+//     return `Product with id ${id} updated!`;
+//   }
+// }
