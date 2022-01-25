b18167d60286245f8959a875711131e147be11bb
C1neks <baranowskimarcin7@gmail.com>
2022-01-25 17:11:42 +0100

    cleans code

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,46 +1,5 @@
 import mongoose from "mongoose";
 
-// const orderSchema = new mongoose.Schema(
-//   {
-//     products: [
-//       {
-//         type: mongoose.Schema.Types.ObjectId,
-//         ref: "Product",
-//         quantity: Number,
-//       },
-//     ],
-//     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-//     quantity: { type: Number, default: 1 },
-//     price: { type: Number, default: 0 },
-//     status: { type: Number, default: 0 },
-//   },
-//   { timestamps: { createdAt: "created_at" } }
-// );
-let ItemSchema = new mongoose.Schema(
-  {
-    productId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Product",
-      required: true,
-    },
-    quantity: {
-      type: Number,
-      required: true,
-      min: [1, "Quantity can not be less then 1."],
-    },
-    price: {
-      type: Number,
-      required: true,
-    },
-    total: {
-      type: Number,
-      required: true,
-    },
-  },
-  {
-    timestamps: true,
-  }
-);
 const CartSchema = new mongoose.Schema(
   {
     items: [
