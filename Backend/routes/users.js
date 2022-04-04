9748501baaea89c5085aa19a12fa250377778807
C1neks <baranowskimarcin7@gmail.com>
2022-04-04 12:22:55 +0200

    Last changes with token

--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -12,14 +12,17 @@ export const userService = new UserService(new Repository(User));
 
 const authenticateToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
-  if (token === null) return res.sendStatus(401);
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
-    req.user = user;
-    next();
-  });
+  if (authHeader) {
+    const token = authHeader && authHeader.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+      if (err) return res.sendStatus(403);
+      req.user = user;
+      next();
+    });
+  } else {
+    res.sendStatus(403);
+  }
 };
 
 router.get("/", async (req, res) => {
