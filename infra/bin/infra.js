7b5b48d617105c046f37c8cc7ced57426df9a96d
C1neks <baranowskimarcin7@gmail.com>
2022-04-21 16:05:02 +0200

    Fixes bcrypt error from AWS Lambda

--- infra/bin/infra.js
+++ infra/bin/infra.js
@@ -1,20 +1,10 @@
 #!/usr/bin/env node
-import { config } from "dotenv";
-import { resolve } from "path";
+const { config } = require("dotenv");
+const { resolve } = require("path");
 
 const cdk = require("aws-cdk-lib");
 const { InfraStack } = require("../lib/infra-stack");
 config({ path: resolve(__dirname, "../../Backend/.env") });
 const app = new cdk.App();
-new InfraStack(app, "InfraStack", {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+new InfraStack(app, "InfraStack");
+
