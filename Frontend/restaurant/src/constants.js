6bd7fad20226a5c904d508420dccc7780cc56cb3
C1neks <baranowskimarcin7@gmail.com>
2022-05-15 20:01:43 +0200

    Deploying Frontend and Backend to my InfraStack

--- Frontend/restaurant/src/constants.js
+++ Frontend/restaurant/src/constants.js
@@ -1,4 +1,3 @@
-export const ENDPOINT_RESTAURANT = `http://localhost:4000`;
-// export const ENDPOINT_RESTAURANT = `https://5zz4tsek94.execute-api.eu-central-1.amazonaws.com/prod/`;
+export const ENDPOINT_RESTAURANT = `https://7ii24q1xu8.execute-api.eu-central-1.amazonaws.com/prod`;
 
 export const IMAGE_ENDPOINT = `${ENDPOINT_RESTAURANT}/static`;
