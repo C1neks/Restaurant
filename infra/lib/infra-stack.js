d01bbc354fcc9e325b3180b155a7fa63656267b4
C1neks <baranowskimarcin7@gmail.com>
2022-05-15 20:01:43 +0200

    Deploying Frontend and Backend to my InfraStack

--- infra/lib/infra-stack.js
+++ infra/lib/infra-stack.js
@@ -4,13 +4,14 @@ const S3 = require("aws-cdk-lib/aws-s3");
 const S3Deployment = require("aws-cdk-lib/aws-s3-deployment");
 const lambda = require("aws-cdk-lib/aws-lambda-nodejs");
 const gateway = require("aws-cdk-lib/aws-apigateway");
+const { SPADeploy } = require("cdk-spa-deploy");
 
 class InfraStack extends Stack {
   constructor(scope, id, props) {
     super(scope, id, props);
-    const bucket = new S3.Bucket(this, "myImageBucket", {
-      bucketName: "resturant.image.bucket",
-    });
+    // const bucket = new S3.Bucket(this, "myImageBucket", {
+    //   bucketName: "resturant.image.bucket",
+    // });
     // new S3Deployment.BucketDeployment(this, "Deployment", {
     //   sources: [
     //     S3Deployment.Source.asset(__dirname + "../../../Backend/public"),
@@ -35,6 +36,10 @@ class InfraStack extends Stack {
       handler: restaurantApiLambda,
     });
     console.log(api.restApiName);
+    new SPADeploy(this, "my-restaurant-app").createSiteWithCloudfront({
+      indexDoc: "index.html",
+      websiteFolder: "../../Restaurant/Frontend/restaurant/build",
+    });
   }
 }
 
