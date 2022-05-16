c5282efaf84ac09a3a418d39b021e6c17ef06b05
C1neks <baranowskimarcin7@gmail.com>
2022-05-16 16:34:38 +0200

    Updates AWS endpoint

--- Frontend/restaurant/src/constants.js
+++ Frontend/restaurant/src/constants.js
@@ -1,3 +1,3 @@
-export const ENDPOINT_RESTAURANT = `https://7ii24q1xu8.execute-api.eu-central-1.amazonaws.com/prod`;
+export const ENDPOINT_RESTAURANT = `https://t0g9aoe2uc.execute-api.eu-central-1.amazonaws.com/prod/`;
 
 export const IMAGE_ENDPOINT = `${ENDPOINT_RESTAURANT}/static`;
