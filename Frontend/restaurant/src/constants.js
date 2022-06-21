cbcc2779a3b6f3a98f3b03eb00ddfb1631bac3c9
C1neks <baranowskimarcin7@gmail.com>
2022-06-21 21:42:28 +0200

    Const change

--- Frontend/restaurant/src/constants.js
+++ Frontend/restaurant/src/constants.js
@@ -1,5 +1,5 @@
-// export const ENDPOINT_RESTAURANT = `https://qztonf999i.execute-api.eu-central-1.amazonaws.com/prod/`;
-export const ENDPOINT_RESTAURANT = `http://localhost:4000`;
+export const ENDPOINT_RESTAURANT = `https://qztonf999i.execute-api.eu-central-1.amazonaws.com/prod/`;
+// export const ENDPOINT_RESTAURANT = `http://localhost:4000`;
 export const ENDPOINT_IMAGE_RESTAURANT = `http://localhost:4000`;
 
 export const IMAGE_ENDPOINT = `${ENDPOINT_IMAGE_RESTAURANT}/static`;
